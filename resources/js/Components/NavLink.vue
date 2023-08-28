a466fd69bd5add2092b52047b2f29b2236262601
Clayton Faria <claytonfaria@gmail.com>
2023-08-28 20:16:51 +0800

    Enhanced contrast for NavLink in dark mode
    Updated the class attributes in the NavLink.vue file to improve contrast and enhance readability when in dark mode. This was done by adjusting the background and text colours.

--- resources/js/Components/NavLink.vue
+++ resources/js/Components/NavLink.vue
@@ -12,8 +12,10 @@ defineProps({
 <template>
   <Link
     :href="href"
-    :class="{ 'text-purple-800 dark:text-purple-400 bg-gray-200': active }"
-    class="flex items-center rounded-md text-sm p-2 gap-x-2.5 text-gray-500 dark:text-gray-400 hover:bg-gray-200 dark:hover:bg-gray-800 hover:text-gray-800 font-semibold dark:hover:text-gray-300"
+    :class="{ 'text-purple-800 dark:text-purple-400 bg-gray-200 dark:bg-gray-700': active }"
+    class="flex items-center rounded-md text-sm p-2 gap-x-2.5 text-gray-500
+     dark:text-gray-400 hover:bg-gray-200 dark:hover:bg-gray-800 hover:text-purple-800
+      dark:hover:text-purple-400 font-semibold "
   >
     <Icon :icon="icon" class="w-6 h-6" />
 
